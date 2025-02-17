90472ea432ebe3047942686b1e3ee92ea2014d35
apple-marie <am.trilles@mlgcl.edu.ph>
2025-02-17 21:03:29 +0800

    02-17-2025

--- database/migrations/2025_02_17_113156_create_students_table.php
+++ database/migrations/2025_02_17_113156_create_students_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            // $table->string('email');
-            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
