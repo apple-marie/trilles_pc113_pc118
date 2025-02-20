01cd36ce163ba4e70311192adc382fe1befb2219
apple-marie <am.trilles@mlgcl.edu.ph>
2025-02-20 14:28:29 +0800

    2/17/2025 Activity 1

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Employee;
+
+class ListController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Student::all());
+            
+    }
+
+    public function employee()
+    {
+        return response()->json(Employee::all());
+            
+    }
+}
